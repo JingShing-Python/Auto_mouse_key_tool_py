0855b37d986d94b5c2d8b8e443c11368b454f21d
JingShing <JingShingLai@gmail.com>
2023-05-16 00:09:37 +0800

    Update auto_mouse_key.py

--- auto_mouse_key/auto_mouse_key.py
+++ auto_mouse_key/auto_mouse_key.py
@@ -5,21 +5,40 @@ import time
 def process_command(command):
     action, *args = command.split()
 
-    key_actions = ['key']
-    mouse_actions = ['move', 'click']
-    click_types = ['right', 'left']
+    key_actions = ['key', 'hold', 'release']
+    mouse_actions = ['move', 'click', 'scroll']
+    click_types = ['left', 'right']
+    scroll_directions = ['up', 'down']
 
     if action in key_actions:
         # keyboard action
         # key string
-        keyboard.write(' '.join(args))
+        if action == 'key':
+            keyboard.write(' '.join(args))
+        elif action == 'hold':
+            key = args[0]
+            keyboard.press(key)
+        elif action == 'release':
+            key = args[0]
+            keyboard.release(key)
     elif action in mouse_actions:
         # mouse action
+
+        # move
         # move relative(r) dx dy
         # move absolute(a) x y
         # move image(i) image_path
-        # click right
-        # click left
+
+        # click
+        # click left/right/middle
+        # click left/right/middle N
+        # click left/right/middle hold
+        # click left/right/middle release
+        # click left/right/middle hold release
+
+        # scroll
+        # scroll up/down
+        # scroll up/down amount
         if action == 'move':
             move_type, *values = args
             if move_type == 'relative' or move_type == 'r':
@@ -38,9 +57,27 @@ def process_command(command):
                     x, y = center
                     pyautogui.moveTo(x, y)
         elif action == 'click':
-            click_type = args[0]
-            if click_type in click_types:
-                pyautogui.click(button=click_type)
+            if 'hold' in args:
+                pyautogui.mouseDown()
+                if 'release' in args:
+                    time.sleep(0.1)
+                    pyautogui.mouseUp()
+            elif 'release' in args:
+                pyautogui.mouseUp()
+            else:
+                click_type = args[0]
+                if click_type in click_types:
+                    count = int(args[1]) if len(args) > 1 else 1
+                    for _ in range(count):
+                        pyautogui.click(button=click_type)
+        elif action == 'scroll':
+            scroll_direction = args[0]
+            if scroll_direction in scroll_directions:
+                amount = int(args[1]) if len(args) > 1 else 1
+                if scroll_direction == 'up':
+                    pyautogui.scroll(amount)
+                elif scroll_direction == 'down':
+                    pyautogui.scroll(-amount)
     elif action == 'sleep':
         time.sleep(int(args[-1]))
 
