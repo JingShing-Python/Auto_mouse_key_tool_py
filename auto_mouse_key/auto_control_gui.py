e9b9be11d70b0e5c86c786064a6ff5e430a7eddd
JingShing <JingShingLai@gmail.com>
2023-05-16 19:15:09 +0800

    Update and rename gui.py to auto_control_gui.py

--- auto_mouse_key/auto_control_gui.py
+++ auto_mouse_key/auto_control_gui.py
@@ -3,13 +3,95 @@ from tkinter import filedialog
 from tkinter import messagebox
 import threading
 import time
-from auto_control import process_command, skip_else_block
 import pyautogui
 import keyboard
 
 instructions = []
 file_path = ""
 is_running = False
+
+def process_command(command):
+    print(command)
+    parts = command.split()
+    if len(parts) > 0:
+        action, *args = parts
+        key_actions = ['key', 'hold', 'press', 'release']
+        mouse_actions = ['move', 'click', 'scroll']
+        click_types = ['left', 'right']
+        scroll_directions = ['up', 'down']
+    
+        if action in key_actions:
+            # keyboard action
+            if action == 'key':
+                keyboard.write(' '.join(args))
+            elif action == 'press':
+                key = ' '.join(args)
+                keyboard.press(key)
+                keyboard.release(key)
+            elif action == 'hold':
+                key = ' '.join(args)
+                keyboard.press(key)
+            elif action == 'release':
+                key = ' '.join(args)
+                keyboard.release(key)
+        elif action in mouse_actions:
+            # mouse action
+            if action == 'move':
+                move_type, *values = args
+                if move_type == 'relative' or move_type == 'r':
+                    x, y = map(int, values)
+                    current_x, current_y = pyautogui.position()
+                    target_x = current_x + x
+                    target_y = current_y + y
+                    pyautogui.moveTo(target_x, target_y)
+                elif move_type == 'absolute' or move_type == 'a':
+                    x, y = map(int, values)
+                    pyautogui.moveTo(x, y)
+                elif move_type == 'image' or move_type == 'i':
+                    image_path = ' '.join(values)
+                    center = pyautogui.locateCenterOnScreen(image_path)
+                    if center:
+                        x, y = center
+                        pyautogui.moveTo(x, y)
+            elif action == 'click':
+                if 'hold' in args:
+                    pyautogui.mouseDown()
+                    if 'release' in args:
+                        time.sleep(0.1)
+                        pyautogui.mouseUp()
+                elif 'release' in args:
+                    pyautogui.mouseUp()
+                else:
+                    click_type = args[0]
+                    if click_type in click_types:
+                        count = int(args[1]) if len(args) > 1 else 1
+                        for _ in range(count):
+                            pyautogui.click(button=click_type)
+            elif action == 'scroll':
+                scroll_direction = args[0]
+                if scroll_direction in scroll_directions:
+                    amount = int(args[1]) if len(args) > 1 else 1
+                    if scroll_direction == 'up':
+                        pyautogui.scroll(amount)
+                    elif scroll_direction == 'down':
+                        pyautogui.scroll(-amount)
+        elif action == 'sleep':
+            time.sleep(int(args[-1]))
+    else:
+        print("Invalid command: No action specified.")
+
+def skip_else_block(instructions, start_index):
+    nested_if_count = 0
+    nested_endif_count = 1
+    i = start_index + 1
+    while nested_if_count > nested_endif_count:
+        nested_instruction = instructions[i]
+        if 'endif' in nested_instruction:
+            nested_endif_count += 1
+        elif 'if' in nested_instruction:
+            nested_if_count += 1
+        i += 1
+    return i
 
 def load_instructions(old_file_path=None):
     global instructions
